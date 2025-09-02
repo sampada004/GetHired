2daa9a58b5c3536015cfca8682aa90a7062b3807
sampada004 <sampadadubey2006@gmail.com>
2025-09-02 19:59:44 +0530

    error solved

--- src/main/java/com/GetHired/Service/EmployeeServices.java
+++ src/main/java/com/GetHired/Service/EmployeeServices.java
@@ -1,22 +1,21 @@
 package com.GetHired.Service;
 
 import com.GetHired.DAO.EmployeeDAO;
-import com.GetHired.DAO.JobDAO;
+
 import com.GetHired.Entities.Employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+
+
 import java.util.Optional;
 
 @Service
 public class EmployeeServices {
     @Autowired
     private EmployeeDAO employeeDAO;
-    @Autowired
-    private JobDAO jobDAO;
+
 
     public void addEmployee(Employee employee){
         employeeDAO.save(employee);
